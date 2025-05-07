2b166c074da60d07b68050e822d72495bab4e36c
Tesfamariam Gebre <dopelij543@gmail.com>
2025-05-07 12:58:42 +0300

    Gin added and configured

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,19 @@
-package api
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+
+	//set up gin router
+	router := gin.Default()
+	router.Use(gin.Recovery())
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// Start server
+	router.Run(":8080")
+}
